cab392f159669a531732b9ea4da7f4a665fe80ff
Victor Qu <Victor.X.Qu@newegg.com>
2025-10-28 09:21:54 +0800

    JsonPathParser

--- src/Lmzzz.JsonPath/JsonPathParser.cs
+++ src/Lmzzz.JsonPath/JsonPathParser.cs
@@ -1,13 +1,17 @@
 ﻿using Lmzzz.Chars;
 using Lmzzz.Chars.Fluent;
+using Lmzzz.JsonPath.Statements;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Security.AccessControl;
 using static Lmzzz.Chars.Fluent.Parsers;
 
 namespace Lmzzz.JsonPath;
 
 public class JsonPathParser
 {
-    public static Parser<IStatement> JsonPathQuery = RootIdentifier.And(Segments);
-    public static Parser<IStatement> Segments = ZeroOrMany(S.And(segment));
+    public static Parser<IStatement> JsonPathQuery = RootIdentifier.And(Segments).Then<IStatement>(static x => new RootNode() { Child = x.Item2 });
+    public static Parser<IStatement> Segments = ZeroOrMany(S.And(Segment));
 
     public static Parser<char> B = Char(new char[]
     { (char)0x20, // Space
@@ -27,48 +31,61 @@ public class JsonPathParser
     public static Parser<char> WildcardSelector = Char('*');
     public static Parser<decimal> IndexSelector = Int;
     public static Parser<decimal> Int = Decimal(NumberOptions.Integer);
-    public static Sequence<(decimal, IReadOnlyList<char>), char, IReadOnlyList<char>, (decimal, IReadOnlyList<char>), (char, (IReadOnlyList<char>, decimal))> SliceSelector = Optional(Start.And(S)).And(Char(':')).And(S).And(Optional(End.And(S))).And(Optional(Char(':').And(Optional(S.And(Step)))));
+    public static Parser<IStatement> SliceSelector = Optional(Start.And(S)).And(Char(':')).And(S).And(Optional(End.And(S))).And(Optional(Char(':').And(Optional(S.And(Step)))));
     public static Parser<decimal> Start = Int;
     public static Parser<decimal> End = Int;
     public static Parser<decimal> Step = Int;
-    public static Parser<char> FilterSelector = Char('?').And(S).And(LogicalExpr);
-    public static Parser<char> LogicalExpr = LogicalOrExpr;
-    public static Parser<char> LogicalOrExpr = LogicalAndExpr.And(ZeroOrMany(S.And(Text("||")).And(S).And(LogicalAndExpr)));
-    public static Parser<char> LogicalAndExpr = BasicExpr.And(ZeroOrMany(S.And(Text("&&")).And(S).And(BasicExpr)));
-    public static Parser<char> BasicExpr = ParenExpr.Or(ComparisonExpr).Or(TestExpr);
-    public static Parser<char> ParenExpr = Optional(LogicalNotOp.And(S)).And(Char('(')).And(S).And(LogicalExpr).And(S).And(Char(')'));
+
+    public static Parser<IStatement> FilterSelector = Char('?').And(S).And(LogicalExpr).Then<IStatement>(static x => new UnaryOperaterStatement()
+    {
+        Operater = x.Item1.ToString(),
+        Statement = x.Item3
+    });
+
+    public static Parser<IStatement> LogicalExpr = LogicalOrExpr;
+    public static Parser<IStatement> LogicalOrExpr = LogicalAndExpr.And(ZeroOrMany(S.And(Text("||")).And(S).And(LogicalAndExpr)));
+    public static Parser<IStatement> LogicalAndExpr = BasicExpr.And(ZeroOrMany(S.And(Text("&&")).And(S).And(BasicExpr)));
+    public static Parser<IStatement> BasicExpr = ParenExpr.Or(ComparisonExpr).Or(TestExpr);
+
+    public static Parser<IStatement> ParenExpr = Optional(LogicalNotOp.And(S)).And(Char('(')).And(S).And(LogicalExpr).And(S).And(Char(')'))
+        .Then<IStatement>(static x => new UnaryOperaterStatement()
+        {
+            Operater = x.Item1.Item1.ToString(),
+            Statement = x.Item4
+        });
+
     public static Parser<char> LogicalNotOp = Char('!');
-    public static Parser<char> TestExpr = Optional(LogicalNotOp.And(S)).And(FilterQuery.Or(FunctionExpr));
-    public static Parser<char> FilterQuery = RelQuery.Or(JsonPathQuery);
-    public static Parser<char> RelQuery = CurrentNodeIdentifier.And(Segments);
+    public static Parser<char> TestExpr = Optional(LogicalNotOp.And(S)).And(FilterQuery.Or(FunctionExpr)).Then<IStatement>(static x => new UnaryOperaterStatement() { Operater = x.Item1.Item1.ToString(), Statement = x.Item2 });
+    public static Parser<IStatement> FilterQuery = RelQuery.Or(JsonPathQuery);
+    public static Parser<IStatement> RelQuery = CurrentNodeIdentifier.And(Segments).Then<IStatement>(static x => new CurrentNode() { Child = x.Item2 });
     public static Parser<char> CurrentNodeIdentifier = Char('@');
-    public static Parser<char> ComparisonExpr = Comparable.And(S).And(ComparisonOp).And(S).And(Comparable);
-    public static Parser<char> Literal = Num.Or(StringLiteral).Or(True).Or(False).Or(Null);
-    public static Parser<char> Comparable = Literal.Or(SingularQuery).Or(FunctionExpr);
+    public static Parser<IStatement> ComparisonExpr = Comparable.And(S).And(ComparisonOp).And(S).And(Comparable).Then<IStatement>(static x => new OperatorStatement() { Left = x.Item1, Operator = x.Item3, Right = x.Item5 });
+    public static Parser<IStatement> Literal = Num.Or(StringLiteral.Then<IStatement>(static x => new StringValue(x.Span.ToString()))).Or(True).Or(False).Or(Null);
+    public static Parser<IStatement> Comparable = Literal.Or(SingularQuery).Or(FunctionExpr);
     public static Parser<string> ComparisonOp = Text("==").Or(Text("!=")).Or(Text("<=")).Or(Text(">=")).Or(Text("<")).Or(Text(">"));
-    public static Parser<char> SingularQuery = RelSingularQuery.Or(AbsSingularQuery);
-    public static Parser<char> RelSingularQuery = CurrentNodeIdentifier.And(SingularQuerySegments);
-    public static Parser<char> AbsSingularQuery = RootIdentifier.And(SingularQuerySegments);
-    public static Parser<char> SingularQuerySegments = ZeroOrMany(S.And(NameSegment.Or(IndexSegment)));
-    public static Parser<char> NameSegment = Char('[').And(NameSelector).And(Char(']')).Or(Char('.').And(MemberNameShorthand));
+    public static Parser<IStatement> SingularQuery = RelSingularQuery.Or(AbsSingularQuery);
+    public static Parser<IStatement> RelSingularQuery = CurrentNodeIdentifier.And(SingularQuerySegments);
+    public static Parser<IStatement> AbsSingularQuery = RootIdentifier.And(SingularQuerySegments);
+    public static Parser<IStatement> SingularQuerySegments = ZeroOrMany(S.And(NameSegment.Or(IndexSegment)));
+    public static Parser<IStatement> NameSegment = Char('[').And(NameSelector).And(Char(']')).Or(Char('.').And(MemberNameShorthand));
     public static Sequence<char, decimal, char> IndexSegment = Char('[').And(IndexSelector).And(Char(']'));
-    public static Parser<decimal> Num = Decimal(NumberOptions.Float);
-    public static Parser<string> True = Text("true");
-    public static Parser<string> False = Text("false");
-    public static Parser<string> Null = Text("null");
+    public static Parser<IStatement> Num = Decimal(NumberOptions.Float).Then<IStatement>(static x => new NumberValue(x));
+    public static Parser<IStatement> True = Text("true").Then<IStatement>(static x => BoolValue.True);
+    public static Parser<IStatement> False = Text("false").Then<IStatement>(static x => BoolValue.False);
+    public static Parser<IStatement> Null = Text("null").Then<IStatement>(static x => NullValue.Value);
     public static Parser<string> FunctionName = FunctionNameFirst.And(ZeroOrMany(FunctionNameChar));
     public static Parser<char> FunctionNameFirst = LCALPHA;
     public static Parser<char> FunctionNameChar = FunctionNameFirst.Or(Char('_')).Or(DIGIT);
     public static Parser<char> LCALPHA = Char('a', 'z');
-    public static Parser<char> FunctionExpr = FunctionName.And(Char('(')).And(S).And(Optional(FunctionArgument.And(ZeroOrMany(S.And(Char(',')).And(S).And(FunctionArgument))))).And(S).And(Char(')'));
+    public static Parser<IStatement> FunctionExpr = FunctionName.And(Char('(')).And(S).And(Optional(FunctionArgument.And(ZeroOrMany(S.And(Char(',')).And(S).And(FunctionArgument))))).And(S).And(Char(')'));
     public static Parser<char> FunctionArgument = Literal.Or(FilterQuery).Or(LogicalExpr).Or(FunctionExpr);
     public static Parser<char> Segment = ChildSegment.Or(DescendantSegment);
     public static Parser<char> ChildSegment = BracketedSelection.Or(Char('.').And(WildcardSelector.Or(MemberNameShorthand)));
     public static Parser<char> BracketedSelection = Char('[').And(S).And(Selector).And(ZeroOrMany(S.And(Char(',')).And(Selector))).And(S).And(Char(']'));
-    public static Parser<char> MemberNameShorthand = NameFirst.And(ZeroOrMany(NameChar));
+    public static Parser<IStatement> MemberNameShorthand = NameFirst.And(ZeroOrMany(NameChar)).Then<IStatement>(static x => new Member { Name = x.Item1 + new string(x.Item2.ToArray()) });
     public static Parser<char> NameFirst = ALPHA.Or(Char('_')).Or(Char((char)0x80, (char)0xD7FF)).Or(Char((char)0xE000, (char)0xFFFF));
     public static Parser<char> NameChar = NameFirst.Or(DIGIT);
     public static Parser<char> DIGIT = Char('0', '9');
     public static Parser<char> ALPHA = Char((char)0x41, (char)0x5A).Or(Char((char)0x61, (char)0x7A));
-    public static Parser<char> DescendantSegment = Text("..").And(BracketedSelection.Or(WildcardSelector).Or(MemberNameShorthand));
+    public static Parser<char> DescendantSegment = Char('.').And(Char('.')).And(BracketedSelection.Or(WildcardSelector).Or(MemberNameShorthand));
 }
---
+++ src/Lmzzz.JsonPath/Statements/Node.cs
@@ -0,0 +1,72 @@
+﻿namespace Lmzzz.JsonPath.Statements;
+
+public class Node : IStatement
+{
+    public IStatement? Child { get; set; }
+}
+
+public class RootNode : Node
+{
+}
+
+public class CurrentNode : Node
+{
+}
+
+public class Member : IStatement
+{
+    public string Name { get; set; }
+}
+
+public class NumberValue : IStatement
+{
+    public NumberValue(decimal x)
+    {
+        this.Value = x;
+    }
+
+    public decimal Value { get; }
+}
+
+public class BoolValue : IStatement
+{
+    public static readonly BoolValue True = new(true);
+    public static readonly BoolValue False = new(false);
+
+    public BoolValue(bool x)
+    {
+        this.Value = x;
+    }
+
+    public bool Value { get; }
+
+    public static BoolValue From(bool x) => x ? True : False;
+}
+
+public class NullValue : IStatement
+{
+    public static readonly NullValue Value = new();
+}
+
+public class StringValue : IStatement
+{
+    public StringValue(string value)
+    {
+        Value = value;
+    }
+
+    public string Value { get; }
+}
+
+public class OperatorStatement : IStatement
+{
+    public IStatement Left { get; set; }
+    public string Operator { get; set; }
+    public IStatement Right { get; set; }
+}
+
+public class UnaryOperaterStatement : IStatement
+{
+    public string Operator { get; set; }
+    public IStatement Statement { get; set; }
+}
